18461ee2432bd5aa8591453bd8f049ca7f625b41
Kelvin Brito <britok30@gmail.com>
2020-07-07 01:39:43 -0400

    searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+    state = {
+        term: "",
+    };
+
+    onChange = (e) => {
+        return this.setState({ term: e.target.value });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+    };
+
     render() {
-        return <div>App</div>;
+        return (
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <label htmlFor="search" style={{ marginTop: "3rem" }}>
+                        Video Search
+                    </label>
+                    <input
+                        type="text"
+                        name="search"
+                        style={{ display: "block", width: "100%" }}
+                        onChange={this.onChange}
+                        value={this.state.term}
+                    />
+                </form>
+            </div>
+        );
     }
 }
 
