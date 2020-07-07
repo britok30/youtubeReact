efdf5a2879f17d085cf99ddbe35e09563507024e
Kelvin Brito <britok30@gmail.com>
2020-07-07 02:56:40 -0400

    youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import Youtube from "../api/Youtube";
 import "../App.css";
 
+const KEY = "AIzaSyDR385vzb0vgxny4atFafXRSpgXk40UCsQ";
+
 class App extends Component {
+    onTermSubmit = async (term) => {
+        const response = await Youtube.get("/search", {
+            params: {
+                q: term,
+                part: "snippet",
+                type: "video",
+                maxResults: 15,
+                key: KEY,
+            },
+        });
+
+        console.log(response);
+    };
+
     render() {
         return (
             <div className="container">
-                <SearchBar />
+                <SearchBar onFormSubmit={this.onTermSubmit} />
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,24 +9,39 @@ class SearchBar extends Component {
         return this.setState({ term: e.target.value });
     };
 
-    onSubmit = (e) => {
+    onFormSubmit = (e) => {
         e.preventDefault();
+
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
         return (
             <div>
-                <form onSubmit={this.onSubmit}>
+                <form onSubmit={this.onFormSubmit}>
                     <label htmlFor="search" style={{ marginTop: "3rem" }}>
                         Video Search
                     </label>
-                    <input
-                        type="text"
-                        name="search"
-                        style={{ display: "block", width: "100%" }}
-                        onChange={this.onChange}
-                        value={this.state.term}
-                    />
+
+                    <div className="input-group mb-3">
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="search"
+                            style={{ display: "block", width: "90%" }}
+                            onChange={this.onChange}
+                            value={this.state.term}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="submit"
+                                onSubmit={this.onFormSubmit}
+                            >
+                                <i className="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
                 </form>
             </div>
         );
